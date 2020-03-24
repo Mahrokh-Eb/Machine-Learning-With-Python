b6431dd3fa5bc05723c23e1a41c7c3c5a46f89bb
Mahrokh Ebrahimi <59485787+Mahrokh-Eb@users.noreply.github.com>
2020-03-23 22:21:41 -0700

    Gambeler

---
+++ gambeler.py
@@ -0,0 +1,33 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+import sys
+import random
+
+stake = int(sys.argv[1])
+goal = int(sys.argv[2])
+trial= int(sys.argv[3])
+
+
+wins = 0
+bets = 0
+
+
+for t in range(triaL):
+     cashe = stake
+     while cashe = 0 or cahe = goal:
+         bet = bet +1
+         if random.randrange(0, 2):
+             cashe = cashe+1
+         else:
+             cashe= cashe - 1
+
+
+     if cashe == goal:
+         win = win+1
+
+
+
+print(str(100* wins // trial)) 
+print(str(bets // tials))

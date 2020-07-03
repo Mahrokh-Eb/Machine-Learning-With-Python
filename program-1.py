729b8e05177ea1fb141547c8b19a3581e411ec30
Mahrokh Ebrahimi <mahrokhh.ebrahimi@gmail.com>
2020-07-02 20:15:14 -0700

    Add files via upload

--- program-1.py
+++ program-1.py
@@ -1,13 +1,29 @@
 '''
 Programmer: Mahrokh Ebrahimi
-Discroption:Write a program to accept the food expenses of 3 Guests (one at a time). Using Accumulation concept, calculate and display the total expenses from all  3 guests combined. 
-Date:6/29/2020
+Discroption: Write a program using for loop to find the square of numbers from 1-10.
+
+Hint: The operator for square is **
+
+Sample Output:
+
+ number          square
+========================
+   1             1
+   2             4
+   3             9
+   4             16
+   5             25
+   6             36
+   7             49
+   8             64
+   9             81
+   10          100
+Date:6/30/2020
 
 '''
-guest1 = int(input('food expenses of guest1?'))
-guest2 = int(input('food expenses of guest2?'))
-guest3 = int(input('food expenses of guest3?'))
-
-sum = guest1 + guest2 + guest3
-
-print('total expenses from all  3 guests combined = ',sum )
+square = 1
+for i in range(1, 11):
+   print(i,'= ', end = '')
+   square = i ** i 
+   print(square)
+   
--- program-2.py
+++ program-2.py
@@ -1,36 +1,25 @@
 '''
 Programmer: Mahrokh Ebrahimi
-Discroption: Write a program to ask the user to enter the amount spent on electricity and Gas bill for 3 consecutive months. Count and display the number of  months where the user went over budget if the electricity bill is over $45 dollars and Gas bill is over $30 dollars. 
-Date:6/29/2020
-
+Discroption:Write a program using for loop to find the total sum of numbers 1-20. Use accumulation concept.
+Date: 30/6/2020
 '''
-electricity1 = int(input('enter the amount spent on electricity-1'))
-electricity2 = int(input('enter the amount spent on electricity-2'))
-electricity3 = int(input('enter the amount spent on electricity-3'))
-
-gas1 = int(input('enter the amount spent on gas-1'))
-gas2 = int(input('enter the amount spent on gas-2'))
-gas3 = int(input('enter the amount spent on gas-3'))
-
 count = 0
-if electricity1 > 45:
-    count += 1
-    
-if electricity2 > 45:
-    count += 1
-    
-if electricity3 > 45:
-    count += 1
-    
-if gas1 > 30:
-    count += 1
-    
-if gas2 > 30:
-    count += 1
+for i in range(1, 21, 1):
+    print(i, end =' ')
+    count += i
+    print('sum = ', count)
 
-if gas2 > 30:
-    count += 1
+print() 
+print('---------------------------------------------------')    
+print('while loop:')
+
+count = 0   
+sum = 0
+while (count <= 20):
+    print(count, end =' ')
+    sum = sum + count
+    print('sum = ', sum)
+    count +=1
     
-print('count= ', count)
     
-    # TODO: write code...
+    

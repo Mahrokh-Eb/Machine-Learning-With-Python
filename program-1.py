f72c4636469ee734f98c1dc4af9242bfcaab91df
Mahrokh Ebrahimi <mahrokhh.ebrahimi@gmail.com>
2020-07-02 20:13:23 -0700

    Add files via upload

--- program-1.py
+++ program-1.py
@@ -1,12 +1,14 @@
 '''
 Programmer: Mahrokh Ebrahimi
-Discroption: Declare two variables and assign values to these two variables
-and then display the product of these two values.
+Discroption: Have a user to enter a number. Write some conditional statement to check whether the number entered by the user is even or odd.
 Date:6/28/2020
 
 '''
+num = int(input('enter a number'))
 
-x = 10
-y = 1
+if (num % 2 == 0):
+    print('it is even!')
+    
+else:
+    print ('it is odd!')
 
-print (x * y)
--- program-2.py
+++ program-2.py
@@ -1,17 +1,45 @@
 '''
 Programmer: Mahrokh Ebrahimi
-Discroption: Write a program that allows the user to enter values for a salesperson’s base salary, total sales, and commission rate. The program computes and outputs the salesperson’s pay by adding the base salary to the product of the total sales and commission rate. 
+
+Discroption:
+Write a program that asks for the names of three runners and the time it took each of them to ﬁnish a race.
+program should display who came in ﬁrst, second, and third place.
+Think about how many test cases are needed to verify that your problem works correctly.
+
 Date:6/28/2020
+
 '''
 
-baseSalary = int(input('what is your base salery?'))
-totalSales = int(input('what is your total sales?'))
-comissionRate = int(input('what is your comission rate?'))
+runner1 = input('runner 1?')
+time1 = int(input('time?'))
 
-#product of the total sales and commission rate
-p = totalSales * comissionRate
+runner2 = input('runner 2?')
+time2 = int(input('time?'))
 
-#adding base salary
-result = p + baseSalary
+runner3 = input('runner 3?')
+time3 = int(input('time?'))
 
-print(result)
+if (time1 < time2):
+    if (time1 < time3):
+        print( 'runner-1 = '+runner1 + ' cames first ')
+        if (time2 < time3):
+            print( 'runner-2 = '+ runner2 + ' comes second')
+            print('runner-3 = '+ runner3 + ' comes at the end')
+        elif (time3 < time2):
+            print( 'runner-3 = '+ runner3 + ' comes second')
+            print('runner-2 = '+ runner2 + ' comes at the end')
+
+
+elif (time2 < time1):
+    if (time2 < time3):
+        print( 'runner-2 = '+runner2 + ' cames first ')
+        if (time1 < time3):
+            print(runner1 +' comes second')
+            print(runner3 +' comes third')
+        elif (time3 < time1):
+            print(runner3 +' comes second')
+            print(runner1 +' comes third')
+        
+            
+
+        

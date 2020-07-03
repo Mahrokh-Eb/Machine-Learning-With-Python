d5be2e9626f784e4cc325c5629d8f950124f6a4d
Mahrokh Ebrahimi <mahrokhh.ebrahimi@gmail.com>
2020-07-02 20:13:54 -0700

    Add files via upload

--- program-1.py
+++ program-1.py
@@ -1,14 +1,13 @@
 '''
 Programmer: Mahrokh Ebrahimi
-Discroption: Have a user to enter a number. Write some conditional statement to check whether the number entered by the user is even or odd.
-Date:6/28/2020
+Discroption:Write a program to accept the food expenses of 3 Guests (one at a time). Using Accumulation concept, calculate and display the total expenses from all  3 guests combined. 
+Date:6/29/2020
 
 '''
-num = int(input('enter a number'))
+guest1 = int(input('food expenses of guest1?'))
+guest2 = int(input('food expenses of guest2?'))
+guest3 = int(input('food expenses of guest3?'))
 
-if (num % 2 == 0):
-    print('it is even!')
-    
-else:
-    print ('it is odd!')
+sum = guest1 + guest2 + guest3
 
+print('total expenses from all  3 guests combined = ',sum )
--- program-2.py
+++ program-2.py
@@ -1,45 +1,36 @@
 '''
 Programmer: Mahrokh Ebrahimi
-
-Discroption:
-Write a program that asks for the names of three runners and the time it took each of them to ﬁnish a race.
-program should display who came in ﬁrst, second, and third place.
-Think about how many test cases are needed to verify that your problem works correctly.
-
-Date:6/28/2020
+Discroption: Write a program to ask the user to enter the amount spent on electricity and Gas bill for 3 consecutive months. Count and display the number of  months where the user went over budget if the electricity bill is over $45 dollars and Gas bill is over $30 dollars. 
+Date:6/29/2020
 
 '''
+electricity1 = int(input('enter the amount spent on electricity-1'))
+electricity2 = int(input('enter the amount spent on electricity-2'))
+electricity3 = int(input('enter the amount spent on electricity-3'))
 
-runner1 = input('runner 1?')
-time1 = int(input('time?'))
+gas1 = int(input('enter the amount spent on gas-1'))
+gas2 = int(input('enter the amount spent on gas-2'))
+gas3 = int(input('enter the amount spent on gas-3'))
 
-runner2 = input('runner 2?')
-time2 = int(input('time?'))
+count = 0
+if electricity1 > 45:
+    count += 1
+    
+if electricity2 > 45:
+    count += 1
+    
+if electricity3 > 45:
+    count += 1
+    
+if gas1 > 30:
+    count += 1
+    
+if gas2 > 30:
+    count += 1
 
-runner3 = input('runner 3?')
-time3 = int(input('time?'))
-
-if (time1 < time2):
-    if (time1 < time3):
-        print( 'runner-1 = '+runner1 + ' cames first ')
-        if (time2 < time3):
-            print( 'runner-2 = '+ runner2 + ' comes second')
-            print('runner-3 = '+ runner3 + ' comes at the end')
-        elif (time3 < time2):
-            print( 'runner-3 = '+ runner3 + ' comes second')
-            print('runner-2 = '+ runner2 + ' comes at the end')
-
-
-elif (time2 < time1):
-    if (time2 < time3):
-        print( 'runner-2 = '+runner2 + ' cames first ')
-        if (time1 < time3):
-            print(runner1 +' comes second')
-            print(runner3 +' comes third')
-        elif (time3 < time1):
-            print(runner3 +' comes second')
-            print(runner1 +' comes third')
-        
-            
-
-        
+if gas2 > 30:
+    count += 1
+    
+print('count= ', count)
+    
+    # TODO: write code...
